f3fa32bc160fdde89fb685d960de2af993be7c21
Vivek <vivekmchaure@gmail.com>
2020-10-03 11:10:51 +0530

    added lag units

--- forecast.py
+++ forecast.py
@@ -2,17 +2,21 @@ import pandas as pd
 from math import inf
 from statsmodels.tsa.ar_model import AutoReg
 
+
 # A US manufacturer buys raw materials in multiple currencies
 purchases = pd.read_excel('Purchases.xlsx')
 
 # For each of those currencies, find the best model to forecast prices
 best_model = {}
+
+LAG_IN_DAYS = (3, 5, 7, 10, 14, 28, 60, 90, 120, 183, 365, 730, 1095)
+
 for currency in purchases.currency:
     data = pd.read_excel(f'{currency}.xlsx')
     data = data[data[currency] > 0]
     best_aic, best_fit = inf, None
-    for lags in (3, 5, 7, 10, 14, 28, 60, 90, 120, 183, 365, 730, 1095):
-        model = AutoReg(data[currency], lags=lags)
+    for lags in LAG_IN_DAYS:
+        model = AutoReg(data[currency], lags=lags, old_names=False)
         fit = model.fit()
         if fit.aic < best_aic:
             best_aic, best_fit = fit.aic, fit
